b54f76df99d7cc758be0c247a27b976c2ae1c80a
ramosisw <ramos.isw@gmail.com>
2019-06-12 04:14:16 +0000

    Updates

---
+++ precache-manifest.beff45f011a471283334c9234005b00c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d35dd07f60b1a3d3ef96030e69b499",
+    "url": "/portify/index.html"
+  },
+  {
+    "revision": "fb0c44abc793ef96b4fd",
+    "url": "/portify/static/js/2.5461c3eb.chunk.js"
+  },
+  {
+    "revision": "332877e2b4259f57138c",
+    "url": "/portify/static/js/main.a2af7b88.chunk.js"
+  },
+  {
+    "revision": "705c76d6c24e33c1e9b3",
+    "url": "/portify/static/js/runtime~main.2aff87f6.js"
+  }
+]);
