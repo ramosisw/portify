58e90cc0c1fe810c3655b6051cc0df288cacc375
Julio C. Ramos <ramos.isw@gmail.com>
2019-06-12 00:14:48 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const config = {
     spotifyApi: {
         url: "https://api.spotify.com/v1",
         scope: "user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private user-library-read user-library-modify playlist-read-collaborative",
-        redirect_uri: isDevelopment ? "http://localhost:3000/callback" : "https://ramosisw.github.io/portify/callback",
+        redirect_uri: isDevelopment ? "http://localhost:3000/callback" : "https://ramosisw.github.io/portify",
         client_id: "ba0034384cb24045a078ea5ceae26480"
     }
 };
